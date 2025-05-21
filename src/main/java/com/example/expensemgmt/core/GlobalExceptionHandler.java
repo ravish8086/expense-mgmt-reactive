69a6c0523a9b1b7f33aa5c8785bae1d0f27a0178
Ravi Shankar <ravish.8086@gmail.com>
2025-05-21 12:04:56 -0500

    Fix the global exception handler

--- src/main/java/com/example/expensemgmt/core/GlobalExceptionHandler.java
+++ src/main/java/com/example/expensemgmt/core/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.example.expensemgmt.core;
 
+import com.example.expensemgmt.exception.BadRequestException;
 import com.example.expensemgmt.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -14,25 +14,35 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
 
-        @ExceptionHandler(ResourceNotFoundException.class)
-        public Mono<ResponseEntity<Map<String, Object>>> handleNotFound(ResourceNotFoundException ex) {
-            Map<String, Object> response = Map.of(
-                    "timestamp", LocalDateTime.now(),
-                    "status", HttpStatus.NOT_FOUND.value(),
-                    "error", ex.getMessage()
-            );
-            return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(response));
-        }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFound(ResourceNotFoundException ex) {
+        Map<String, Object> response = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.NOT_FOUND.value(),
+                "error", ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFound(BadRequestException ex) {
+        Map<String, Object> response = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "error", ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 
     @ExceptionHandler(Exception.class)
-    public Mono<ResponseEntity<Map<String, Object>>> handleGeneral(Exception ex) {
-        return Mono.just(new ResponseEntity<>(
+    public ResponseEntity<Map<String, Object>> handleGeneral(Exception ex) {
+        return new ResponseEntity<>(
                 Map.of(
                         "timestamp", LocalDateTime.now(),
                         "status", HttpStatus.INTERNAL_SERVER_ERROR.value(),
                         "error", ex.getMessage()
                 ), HttpStatus.INTERNAL_SERVER_ERROR
-        ));
+        );
     }
 
 }
